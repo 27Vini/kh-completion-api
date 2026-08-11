e94efd9c18b1ca9d0530947841df1986e0c4226c
Vini <vouverney27@gmail.com>
2026-08-11 17:04:13 -0300

    feat: add getAll route with specification to make WHEREs easier to do

--- src/main/java/com/vinicius/khcompletion/controllers/EnemyController.java
+++ src/main/java/com/vinicius/khcompletion/controllers/EnemyController.java
@@ -1,10 +1,24 @@
 package com.vinicius.khcompletion.controllers;
 
+import com.vinicius.khcompletion.dtos.responses.EnemyResponse;
+import com.vinicius.khcompletion.services.EnemyService;
 import com.vinicius.khcompletion.util.Constants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(Constants.API + "enemies")
+@RequiredArgsConstructor
 public class EnemyController {
+    private final EnemyService enemyService;
+
+    @GetMapping
+    public List<EnemyResponse> getAll(@RequestParam(required = false) Long worldId, @RequestParam(required = false) Long itemId) {
+        return enemyService.getAll(worldId, itemId);
+    }
 }
--- src/main/java/com/vinicius/khcompletion/services/EnemyService.java
+++ src/main/java/com/vinicius/khcompletion/services/EnemyService.java
@@ -3,10 +3,13 @@ package com.vinicius.khcompletion.services;
 import com.vinicius.khcompletion.dtos.responses.EnemyResponse;
 import com.vinicius.khcompletion.entities.Enemy;
 import com.vinicius.khcompletion.repositories.EnemyRepository;
+import com.vinicius.khcompletion.specifications.EnemySpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,8 +18,23 @@ public class EnemyService {
     private final EnemyRepository enemyRepository;
 
     @Transactional(readOnly = true)
-    public List<EnemyResponse> getAll(){
-        return this.enemyRepository.findAll()
+    public List<EnemyResponse> getAll(
+            Long worldId,
+            Long itemId
+    ) {
+        List<Specification<Enemy>> filters = new ArrayList<>();
+
+        if (worldId != null) {
+            filters.add(EnemySpecification.hasWorldId(worldId));
+        }
+
+        if (itemId != null) {
+            filters.add(EnemySpecification.hasDropItemId(itemId));
+        }
+
+        Specification<Enemy> specification = Specification.allOf(filters);
+
+        return enemyRepository.findAll(specification)
                 .stream()
                 .map(EnemyResponse::fromEntity)
                 .toList();
